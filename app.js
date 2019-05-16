08fe82eb6ec6213e9b9ed66ad538a82d4bd2612c
lizcampe <lizcampe@gmail.com>
2019-05-16 12:21:54 -0500

    va segundo con middleware

--- app.js
+++ app.js
@@ -30,6 +30,25 @@ app.patch('/partyhard', (req, res)=> {
 	res.status(401). send ("errorazo 401")
 });
 
+app.get('/casita', (req,res)={ 
+	res.send ("Aqui va todo lo que se usará");
+	});
+
+app.get('/casita', (req,res)={ 
+	res.send ("no hay lugar como el hogar");
+	});
+
+app.all ('*', (req, res, next) => { 
+	console.log ('liz hizo like a ', req.path)
+	next() 
+	});
+
+app.all ('*', (req, res, next) => { 
+	console.log ('Podria ser que esté en ', req.path)
+	next() 
+	});
+
+
 app.listen(port, () => console.log ('el servidor esta corriendo'));	
 
 
