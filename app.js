388fe1da08221625c9d202fb64a083b267b90da7
lizcampe <lizcampe@gmail.com>
2019-05-16 17:06:33 -0500

    va la parte que no está saliendo

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const port = 3000;
 const path= require('path')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
+
 let books = [];
 
 app.use(bodyParser.json())
@@ -70,6 +72,35 @@ console.log (req.body);
 
 
 
-app.listen(port, () => console.log ('el servidor esta corriendo'));	
+
+app.post ('/auth/singin', (req,res) => {
+	console.log ('req.body', req.body);
+	if (!(req.body.user && req.body.pass)) {
+		res.status (400).send('se necesita usuario y contraseña')
+	}
+jwt.sign({ user: req.body.user, theme: 'black' }, privateKey, function(err, token) {
+    if(err) {
+      res.send(500).end();
+    } else {
+      res.status(200).send({token: token})
+    }
+  });
+
+app.use((req, res, next)=> {
+	jwt.verify(req.headers.authorization, privateKey, function(err, decoded) {
+    if(err) {
+      res.status(500).end('aqui')
+    } else {
+      console.log(decoded)
+      // checar ese usuario en la base datos a ver si existe
+    }
+    
+  });
+
+app.get ('/taquito', (req, res) => {
+	res.send('si hay')
+});
 
 
+app.listen(port, () => console.log ('el servidor esta corriendo'));
+});
