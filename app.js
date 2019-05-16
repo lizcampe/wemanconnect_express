3703d8ea11eeb6f7fafd7ad2650905a1fd6b85b7
lizcampe <lizcampe@gmail.com>
2019-05-16 12:00:44 -0500

    va la parte

--- app.js
+++ app.js
@@ -16,6 +16,20 @@ res.send('Bye, Bye');
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
+
+
+app.get('/liz', (req, res)=> {
+	res.status(266). send ("hola liz")
+});
+
+app.post('/trabajo', (req, res)=> {
+	res.status(266). send ({foo:"indat"});
+})
+
+app.patch('/partyhard', (req, res)=> {
+	res.status(401). send ("error 401")
+});
+
 app.listen(port, () => console.log ('el servidor esta corriendo'));	
 
 
