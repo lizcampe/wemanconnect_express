5f2bd464cece2ea7924467911cf3591b59c9ae83
lizcampe <lizcampe@gmail.com>
2019-05-16 15:35:06 -0500

    va la parte con más cosas

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require ('express');
 
-
 const app = express();
 const port = 3000;
 const path= require('path')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+let books = [];
 
+app.use(bodyParser.json())
+app.use (cookieParser ())
 
 app.all ('/hello', (req, res) => {
 	res.send('Hello World');
@@ -17,7 +21,6 @@ res.send('Bye, Bye');
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
 
-
 app.get('/liz', (req, res)=> {
 	res.status(266). send ("hola liz")
 });
@@ -25,6 +28,16 @@ app.get('/liz', (req, res)=> {
 app.post('/trabajo', (req, res)=> {
 	res.status(266). send ({foo:"indat"});
 })
+
+app.all ('*', (req, res, next) => { 
+	console.log ('liz hizo like a ', req.path)
+	next() 
+	});
+
+app.all ('*', (req, res, next) => { 
+	console.log ('Podria ser que esté en ', req.path)
+	next() 
+	});
 
 app.patch('/partyhard', (req, res)=> {
 	res.status(401). send ("errorazo 401")
@@ -38,15 +51,22 @@ app.get('/casita', (req,res)=>{
 	res.send ("no hay lugar como el hogar");
 	});
 
-app.all ('*', (req, res, next) => { 
-	console.log ('liz hizo like a ', req.path)
-	next() 
-	});
 
-app.all ('*', (req, res, next) => { 
-	console.log ('Podria ser que esté en ', req.path)
-	next() 
-	});
+app.get('/books', (req, res) => {
+res.status(200).send({data:books});
+	})
+
+app.post ('/books', (req, res) => {
+console.log (req.body);
+
+	if (req.body.book && req.body.author) {
+		books.push(req.body);
+		res.status(201).send ("ya quedo");
+	} else {
+		resp.status (400).send ({error: "you need to pass book and author"})
+	}
+})
+
 
 
 app.listen(port, () => console.log ('el servidor esta corriendo'));	
